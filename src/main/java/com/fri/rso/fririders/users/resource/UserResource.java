b2fcd151ef46c746085bd8dc6deadea7e738e635
Tomi Sebjanic <ts4293@student.uni-lj.si>
2017-11-27 08:01:06 +0100

    Fix log config

--- src/main/java/com/fri/rso/fririders/users/resource/UserResource.java
+++ src/main/java/com/fri/rso/fririders/users/resource/UserResource.java
@@ -13,7 +13,6 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
-import java.util.logging.Logger;
 
 @RequestScoped
 @Consumes(MediaType.APPLICATION_JSON)
